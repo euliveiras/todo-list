4240e0004072424e3f3a48f06b632e77e5dfb652
euliveiras <matheus.skm@hotmail.com>
2023-03-24 20:38:20 -0300

    refactor: added error message

--- frontend/app/routes/tasks/new/index.tsx
+++ frontend/app/routes/tasks/new/index.tsx
@@ -1,8 +1,8 @@
-import { Button } from "@mui/material";
+import { Alert, Button } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import type { ActionArgs, LinksFunction } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { redirect, json } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
 import updateTaskCss from "../../../styles/update-task.css";
 
 export const links: LinksFunction = () => {
@@ -10,32 +10,35 @@ export const links: LinksFunction = () => {
 };
 
 export const action = async ({ request }: ActionArgs) => {
-    const form = await request.formData();
-    const label = form.get("label");
+    const body = await request.formData();
+    const values = Object.fromEntries(body);
     const categories = ["Personal", "Business"];
     const category = categories[Math.floor(Math.random() * categories.length)];
 
-    const body = JSON.stringify({
-        label,
+    const data = JSON.stringify({
+        ...values,
         expiration: new Date(),
         category,
-        ownerId: "3ac4f5a9-44cd-464f-8897-9cec1f0e5ebf",
+        ownerId: process.env.OWNER_ID,
     });
 
-    const response = await fetch(`http://localhost:3000/tasks`, {
+    const response = await fetch(`${process.env.API_ADDRESS}/tasks`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json;charset=utf-8",
         },
-        body,
+        body: data,
     });
 
-    console.log(response);
+    if (response.status === 400) {
+        return json({ ok: false, message: "label must contain 2 or more characters" });
+    }
 
     return redirect("/");
 };
 
 export default function TaskRoute() {
+    const data = useActionData<typeof action>();
     return (
         <Paper className="container">
             <h1>Create task</h1>
@@ -47,6 +50,7 @@ export default function TaskRoute() {
                 <Button variant="contained" type="submit">
                     Save
                 </Button>
+                {data && !data?.ok && <Alert severity="error">{data?.message}</Alert>}
             </Form>
         </Paper>
     );
