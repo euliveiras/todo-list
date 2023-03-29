8a1ba36f3963e6a73c91d71cab7969f4da020245
euliveiras <matheus.skm@hotmail.com>
2023-03-28 21:56:06 -0300

    test: added filter to find-many-tasks

--- backend/src/application/tasks/use-cases/find-many-tasks-by-owner.ts
+++ backend/src/application/tasks/use-cases/find-many-tasks-by-owner.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { Task } from '../entities/task';
 import { TasksRepository } from '../repositories/task-repository';
 
+type FilterOptions = {
+  expiration: Date;
+};
+
 @Injectable()
 export class FindManyTasksByOwner {
   constructor(private tasksRepository: TasksRepository) {}
 
-  async execute(id: string): Promise<Task[]> {
-    const tasks = await this.tasksRepository.findManyTasksByOwnerId(id);
+  async execute(id: string, filter?: FilterOptions): Promise<Task[]> {
+    const tasks = await this.tasksRepository.findManyTasksByOwnerId(id, filter);
 
     return tasks;
   }
